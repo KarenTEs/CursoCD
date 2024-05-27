10190d887a196ad28323b458f1dd2e73401c61c5
KarenTEs <flaca.marzo@gmail.com>
2024-05-26 18:24:25 -0600

    Modificacion ejemplo 1

--- Ejemplo1.R
+++ Ejemplo1.R
@@ -5,7 +5,7 @@ data(NHANES)
 tab <- NHANES %>% filter(AgeDecade == " 20-29" & Gender == "female") %>% summarize(average = mean(BPSysAve, na.rm = TRUE), standard_deviation = sd(BPSysAve, na.rm = TRUE))
 NHANES %>%
 filter(AgeDecade == " 20-29"  & Gender == "female") %>% summarize(minbp = min(BPSysAve, na.rm = TRUE), maxbp = max(BPSysAve, na.rm = TRUE))
-NHANES %>%
- filter(Gender == "female") %>% group_by(AgeDecade) %>% summarize(average = mean(BPSysAve, na.rm = TRUE), standard_deviation = sd(BPSysAve, na.rm = TRUE))
 ##NHANES %>%
-  ##filter(Gender == "male" & AgeDecade == " 40-49") %>% group_by(Race1) %>% summarize(average = mean(BPSysAve, na.rm = TRUE), standard_deviation = sd(BPSysAve, na.rm = TRUE)) %>% arrange(desc(average))
+ ##filter(Gender == "female") %>% group_by(AgeDecade) %>% summarize(average = mean(BPSysAve, na.rm = TRUE), standard_deviation = sd(BPSysAve, na.rm = TRUE))
+NHANES %>%
+  filter(Gender == "male" & AgeDecade == " 40-49") %>% group_by(Race1) %>% summarize(average = mean(BPSysAve, na.rm = TRUE), standard_deviation = sd(BPSysAve, na.rm = TRUE)) %>% arrange(desc(average))
